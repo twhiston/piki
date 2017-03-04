59a14156ccbf56b2626d7971a622915c4a68bac5
Tom Whiston <tom.whiston@pwc-digital.ch>
2017-03-05 00:00:36 +0100

    tidy hostname

--- cmd/hostname.go
+++ cmd/hostname.go
@@ -29,13 +29,8 @@ import (
 // hostnameCmd represents the hostname command
 var hostnameCmd = &cobra.Command{
 	Use:   "hostname",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Set your Piki machine hostname",
+	Long: ``,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		currentHost := helpers.GetFileAsString("/etc/hostname")
@@ -67,14 +62,5 @@ to quickly create a Cobra application.`,
 func init() {
 	RootCmd.AddCommand(hostnameCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
 	hostnameCmd.PersistentFlags().String("name", "piki", "Hostname for the device")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// hostnameCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
