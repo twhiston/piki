714d79f33741368c467081dfbaeba8737bac1c63
Tom Whiston <tom.whiston@gmail.com>
2017-03-05 03:10:01 +0000

    fix server edit function

--- cmd/reboot.go
+++ cmd/reboot.go
@@ -17,7 +17,6 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"github.com/twhiston/piki/helpers"
-	"syscall"
 )
 
 // rebootCmd represents the reboot command
@@ -31,7 +30,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		helpers.RunScript("/sbin/reboot", )
+		helpers.RunScript("/bin/sh", "-c", "sudo /sbin/reboot" )
 
 	},
 }
